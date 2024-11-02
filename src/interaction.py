a147a448768539eff111751b7791587002b97551
Oleg Timer... <olegtimer@yandex.ru>
2024-11-02 17:57:28 +0500

    rename project

---
+++ src/interaction.py
@@ -0,0 +1,37 @@
+import requests
+
+from abc import ABC, abstractmethod
+
+
+class Parser(ABC):
+    """Абстрактный класс определяет подключение к API и получение вакансий."""
+
+    @abstractmethod
+    def load_vacancies(self, keyword):
+        pass
+
+    @abstractmethod
+    def connection(self):
+        pass
+
+
+class HH(Parser):
+    """
+    Класс для работы с API HeadHunter
+    Класс Parser является родительским классом, который вам необходимо реализовать
+    """
+
+    def __init__(self, file_worker):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+        super().__init__(file_worker)
+
+    def load_vacancies(self, keywo rd):
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
