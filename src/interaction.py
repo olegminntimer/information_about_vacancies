7d72ccf3523cf2345e0d6714d82eaa930ad838b8
Oleg Timer... <olegtimer@yandex.ru>
2024-11-03 19:01:27 +0500

    finish 1

--- src/interaction.py
+++ src/interaction.py
@@ -1,37 +1,53 @@
 import requests
 
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 
 class Parser(ABC):
-    """Абстрактный класс определяет подключение к API и получение вакансий."""
-
+    """Абстрактный класс подключения к API и получения вакансий."""
     @abstractmethod
-    def load_vacancies(self, keyword):
+    def __connection_to_api(self):
+        """Абстрактный метод подключения к API"""
         pass
 
     @abstractmethod
-    def connection(self):
+    def get_vacancies(self, keyword):
+        """Абстрактный метод получения коллекции вакансий"""
         pass
 
 
-class HH(Parser):
+class HeadHunterAPI(Parser):
     """
     Класс для работы с API HeadHunter
-    Класс Parser является родительским классом, который вам необходимо реализовать
     """
-
-    def __init__(self, file_worker):
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
+    def __init__(self): #, file_worker):
+        """Конструктор класса HeadHunter"""
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+        self.__params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
-        super().__init__(file_worker)
+        self._Parser__connection_to_api()
+        super().__init__()#file_worker)
 
-    def load_vacancies(self, keywo rd):
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
+    def _Parser__connection_to_api(self):
+        """Приватный метод подключения к API"""
+        return requests.get(self.__url, headers=self.__headers, params=self.__params)
+
+    def get_vacancies(self, keyword: str="") -> None:
+        """Метод загрузки вакансий с НН"""
+        self.__params['text'] = keyword
+        while self.__params.get('page') != 20:
+            response = self._Parser__connection_to_api()
+            if response.status_code != 200:
+                continue
             vacancies = response.json()['items']
             self.vacancies.extend(vacancies)
-            self.params['page'] += 1
+            self.__params['page'] += 1
+
+# if __name__ == "__main__":
+#     BASE_DIR = Path(__file__).resolve().parent.parent
+#     hh_api = HeadHunterAPI()
+#     hh_api.get_vacancies('python')
+#     for i in range(10):
+#         print(hh_api.vacancies[i])
