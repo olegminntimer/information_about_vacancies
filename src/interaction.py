b9fc4141c5e9e805ed1951d45bf9861d6e35d494
Oleg Timer... <olegtimer@yandex.ru>
2024-11-04 13:40:52 +0500

    add work in vacancy

--- src/interaction.py
+++ src/interaction.py
@@ -1,7 +1,7 @@
 import requests
 
 from abc import ABC, abstractmethod
-from pathlib import Path
+# from pathlib import Path
 
 
 class Parser(ABC):
@@ -21,22 +21,23 @@ class HeadHunterAPI(Parser):
     """
     Класс для работы с API HeadHunter
     """
-    def __init__(self): #, file_worker):
+    def __init__(self):
         """Конструктор класса HeadHunter"""
         self.__url = 'https://api.hh.ru/vacancies'
         self.__headers = {'User-Agent': 'HH-User-Agent'}
         self.__params = {'text': '', 'page': 0, 'per_page': 100}
         self.vacancies = []
         self._Parser__connection_to_api()
-        super().__init__()#file_worker)
+        super().__init__()
 
     def _Parser__connection_to_api(self):
         """Приватный метод подключения к API"""
         return requests.get(self.__url, headers=self.__headers, params=self.__params)
 
-    def get_vacancies(self, keyword: str="") -> None:
+    def get_vacancies(self, keyword: str="") -> list:
         """Метод загрузки вакансий с НН"""
         self.__params['text'] = keyword
+        # print("Прогресс запроса: ", end="")
         while self.__params.get('page') != 20:
             response = self._Parser__connection_to_api()
             if response.status_code != 200:
@@ -44,10 +45,25 @@ class HeadHunterAPI(Parser):
             vacancies = response.json()['items']
             self.vacancies.extend(vacancies)
             self.__params['page'] += 1
+            # print("-", end="")
+        return self.vacancies
+
+    def optimized_list(self) -> list:
+        """Метод выборки полей вакансий: id, название, ссылка, зарплата, валюта, требования"""
+        optim = []
+        for row in self.vacancies:
+            if row["salary"]:
+                salary = f"{row["salary"]["from"]}-{row["salary"]["to"]} {row["salary"]["currency"]}"
+            else:
+                salary = None
+            optim.append({"id": row["id"], "name":row["name"], "url": row["alternate_url"], "salary" : salary, "requirement": row["snippet"]["requirement"]})
+        return optim
 
 # if __name__ == "__main__":
-#     BASE_DIR = Path(__file__).resolve().parent.parent
+#     # BASE_DIR = Path(__file__).resolve().parent.parent
 #     hh_api = HeadHunterAPI()
-#     hh_api.get_vacancies('python')
+#     vacancies_list = hh_api.get_vacancies('python')
+#     vacancies_optimized = hh_api.optimized_list()
+#
 #     for i in range(10):
-#         print(hh_api.vacancies[i])
+#         print(vacancies_optimized[i])
--- src/vacancies.py
+++ src/vacancies.py
@@ -3,7 +3,56 @@
 
 class Vacancy():
     """Класс для работы с вакансиями"""
-    def __init__(self):
-        """Конструктор класса"""
-        pass
+    __slots__ = 'id', 'name', 'url', 'salary', 'requirement'
 
+    def __init__(self, id: str, name: str, url: str, salary: [str|None], requirement: str):
+        """Конструктор класса Vacancy"""
+        self.id = id
+        self.name = name
+        self.url = url
+        self.salary = self.__valid_salary(salary)
+        self.requirement = requirement
+
+    @classmethod
+    def __valid_salary(cls, salary):
+        """Приватный метод проверки зарплаты"""
+        if not salary:
+            return "Зарплата не указана"
+        else:
+            return salary
+
+    @classmethod
+    def __verify_data(cls, some):
+        """Валидация при сравнении вакансий"""
+        if not isinstance(some, Vacancy):
+            raise TypeError("Операнд справа должен иметь тип Vacancy")
+
+        if some.salary:
+            if "-" in some.salary.split()[0]:
+                return int(some.salary.split()[0].split("-")[0])
+            else:
+                return 0
+
+
+    def __eq__(self, other):
+        """Магический метод проверки равенства"""
+        return self.__verify_data(self) == self.__verify_data(other)
+
+    def __lt__(self, other):
+        """Магический метод проверки, что операнд слева меньше операнда справа """
+        return self.__verify_data(self) < self.__verify_data(other)
+
+    def __le__(self, other):
+        """Магический метод проверки, что операнд слева меньше или равен операнду справа """
+        return self.__verify_data(self) <= self.__verify_data(other)
+
+
+if __name__ == "__main__":
+    vacancy1 = Vacancy("108971116", "Python Developer", "<https://hh.ru/vacancy/123456>", None,
+                      "Требования: опыт работы от 3 лет...")
+    print(f"For {vacancy1.name} salary {vacancy1.salary}")
+    vacancy2 = Vacancy("109680570", "Python Developer2", "<https://hh.ru/vacancy/123456>", "100000-150000 руб",
+                      "Требования: опыт работы от 3 лет...")
+    print(f"For {vacancy2.name} salary {vacancy2.salary}")
+
+    print(vacancy1 <= vacancy2)
